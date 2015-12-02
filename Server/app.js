1107e1d7bf7491f80e431d73cba47ae351df123d
Joyce Liu <liujoycec@gmail.com>
2015-12-02 00:30:34 -0800

    Got rid of unnecessary dependencies and unused code in app.js and package.json

--- Server/app.js
+++ Server/app.js
@@ -2,14 +2,12 @@ var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var passport = require('passport');
 
 // Require modules for authentication
 var LocalStrategy = require('passport-local').Strategy;
 var GitHubStrategy = require('passport-github').Strategy;
-var expressSession = require('express-session');
 var BearerStrategy = require('passport-http-bearer').Strategy;
 
 var usersRouter = require('./Routes/users');
@@ -22,18 +20,10 @@ var app = express();
 app.use(logger('dev'));
 app.use(favicon(path.join(__dirname, '../public/images', 'favicon.ico')));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 app.options('/*', handlers.sendOptionsHeader);
 
 app.all('/*', handlers.setCorsHeader);
-
-app.use(expressSession({
-  secret: 'secret',
-  resave: false,
-  saveUninitialized: false
-}));
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -59,24 +49,11 @@ passport.use(new BearerStrategy(strategyUtil.bearerStrategyCallback));
 
 app.use('/auth', usersRouter);
 
-app.use(function(err, req, res, next) {
-  console.error(err);
-  console.error(new Error('404').stack);
-  console.log('next: ', next);
-  next();
-});
-
 app.get('/api/*', handlers.getApi);
 app.post('/create', authHandlers.checkTokenHandler, handlers.postSkeleton);
 app.post('/upvote', authHandlers.checkTokenHandler, handlers.upvote);
 app.post('/addEntry', authHandlers.checkTokenHandler, handlers.addEntry);
 app.post('/editEntry', authHandlers.checkTokenHandler, handlers.editEntry);
-
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
 
 // error handlers
 
@@ -84,6 +61,7 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
+    console.error(new Error((err.status || 500) + '').stack);
     res.sendStatus(err.status || 500);
   });
 }
